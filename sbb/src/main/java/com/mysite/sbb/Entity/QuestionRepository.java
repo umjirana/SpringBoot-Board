58d72c5fc86a364b445b5c67dd79b570694dd6fc
umjirana <umjirana@naver.com>
2023-08-23 00:53:03 +0900

    message

--- sbb/src/main/java/com/mysite/sbb/Entity/QuestionRepository.java
+++ sbb/src/main/java/com/mysite/sbb/Entity/QuestionRepository.java
@@ -1,6 +1,7 @@
 package com.mysite.sbb.Entity;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-
+    Question findBySubjectAndContent(String subject, String content);
+    List<Question> findBySubjectLike(String subject);
 }
